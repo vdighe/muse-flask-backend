a9d65dc6e02b75b4b5b9b4e55ea0612fd830ae0c
Vaishali Dighe-Phanse <vaishphanse@gmail.com>
2020-12-07 17:27:35 -0500

    Flask Backend read with GET/POST

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
-from flask import Flask
+from flask import Flask, jsonify, g
+from flask_cors import CORS
+
+from resources.songs import song # import the song object
+
+import models
 
 DEBUG = True
 PORT = 8000
@@ -7,11 +12,33 @@ PORT = 8000
 # This starts the website!
 app = Flask(__name__)
 
+@app.before_request
+def before_request():
+    """Connect to DB before each request"""
+    g.db = models.DATABASE
+    g.db.connect()
+
+@app.after_request
+def after_request(response):
+    """Close the database connection after each request."""
+    g.db.close()
+    return response
+
+CORS(song, origins=['http://localhost:3000'], supports_credentials=True) # adding this line
+
+app.register_blueprint(song, url_prefix='/api/v1/songs') # adding this line
+
+
 # The default URL ends in / ("my-website.com/").
 @app.route('/')
 def index():
     return 'hi'
 
+@app.route('/json')    
+def muse():
+    return jsonify(name='Vaishali', age='30')
+
 # Run the app when the program starts!
 if __name__ == '__main__':
+    models.initialize()
     app.run(debug=DEBUG, port=PORT)
