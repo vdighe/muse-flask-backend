6c5a59dac953438cf86aee6e87895bd0593edee4
Vaishali Dighe-Phanse <vaishphanse@gmail.com>
2020-12-08 11:44:45 -0500

    Added Update and delete

--- resources/songs.py
+++ resources/songs.py
@@ -16,6 +16,31 @@ def get_all_songs():
     except models.DoesNotExist:
         return jsonify(data={}, status={"code": 401, "message": "Error getting the resources"})
 
+#SHOW ROUTE
+@song.route('/<id>', methods=["GET"])
+def get_one_song(id):
+    print(id, 'reserved word?')
+    song = models.Song.get_by_id(id)
+    print(song.__dict__)
+    return jsonify(data=model_to_dict(song), status={"code": 200, "message": "Success"})
+
+
+#Update Route
+@song.route('/<id>', methods=["PUT"])
+def update_song(id):
+    payload = request.get_json()
+    query = models.Song.update(**payload).where(models.Song.id==id)
+    query.execute()
+    return jsonify(data=model_to_dict(models.Song.get_by_id(id)), status={"code": 200, "message": "resource updated successfully"})
+
+#Delete Route
+@song.route('/<id>', methods=["Delete"])
+def delete_song(id):
+    query = models.Song.delete().where(models.Song.id==id)
+    query.execute()
+    return jsonify(data='resource successfully deleted', status={"code": 200, "message": "resource deleted successfully"})    
+
+
 
 @song.route('/', methods=["POST"])
 def create_songs():
